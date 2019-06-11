36c0c5b0f746656f3c4787e4d95ad0a55b995442
Alex Scott <alex@cgi-central.net>
2019-06-11 13:17:03 +0400

    -

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 docker build -t php-bug-test .
-docker run -p 80:80 php-bug-test
+docker run -d -p 8011:80 php-bug-test
+
+sleep 3
+
+curl http://localhost:8011/
+curl http://localhost:8011/
+
+docker stop $(docker ps -q --filter ancestor=php-bug-test )
